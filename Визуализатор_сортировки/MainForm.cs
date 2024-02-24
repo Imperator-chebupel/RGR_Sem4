78e9f9e8fb43b8e8c7d92acb678981851ec2bb10
Imperator-chebupel <138660337+Imperator-chebupel@users.noreply.github.com>
2024-02-24 18:20:12 +0700

    Вынес презентер

--- Визуализатор_сортировки/MainForm.cs
+++ Визуализатор_сортировки/MainForm.cs
@@ -13,108 +13,51 @@ using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Визуализатор_сортировки
 {
-    public partial class MainForm : Form
+    public partial class MainForm : Form 
     {
         private Presenter P;
         Chart Data = new Chart();
-        System.Windows.Forms.Label label_count = new System.Windows.Forms.Label();
-        System.Windows.Forms.Label label_speed = new System.Windows.Forms.Label();
-        System.Windows.Forms.Button Generate = new System.Windows.Forms.Button();
-        RichTextBox RCB = new RichTextBox();
-        TrackBar Trackbar = new TrackBar();
-        TrackBar Trackbar_ = new TrackBar();
-
+        System.Windows.Forms.Label label_count;
+        System.Windows.Forms.Label label_speed;
+        System.Windows.Forms.Button Generate;
+        RichTextBox RCB;
+        TrackBar Trackbar;
+        TrackBar Trackbar_;
+        CheckBox CB1, CB2;
         public MainForm(Presenter P_)
         {
             P=P_;
-            DrawChart();
-            DrawButton();
-            DrawRichTextBox();
-            DrawLabel();
-            DrawTrackBar();
+
+            Data = P.DrawChart();
+            this.Controls.Add(Data);
+
+            label_count = P.DrawLabel1();
+            this.Controls.Add(label_count);
+
+            label_speed = P.DrawLabel2();
+            this.Controls.Add(label_speed);
+
+            Generate = P.DrawButton();
+            this.Controls.Add(Generate);
+
+            RCB = P.DrawRichTextBox();
+            this.Controls.Add(RCB);
+
+            Trackbar = P.DrawTrackBar_1();
+            this.Controls.Add(Trackbar);
+
+            Trackbar_ = P.DrawTrackBar_2();
+            this.Controls.Add(Trackbar_);
+
+            CB1 = P.DrawCheckBox1();
+            this.Controls.Add(CB1);
+
+            CB2 = P.DrawCheckBox2();
+            this.Controls.Add(CB2);
+
             InitializeComponent();
         }
-
-        private void DrawRichTextBox()
-        {
-            RCB.Location = new Point(520, 60);
-            RCB.Size = new Size(250, 300);
-            this.Controls.Add(RCB);
-        }
-
-        private void DrawTrackBar()
-        {
-            Trackbar.Location = new Point(10,340);
-            Trackbar.Size = new Size(500,200);
-            Trackbar.Minimum = 0;
-            Trackbar.Maximum = 10000;
-            Trackbar.ValueChanged += (sender, e) =>
-            {
-                label_count.Text = "Количество: "+Trackbar.Value.ToString();
-            };
-            Trackbar.TickFrequency = 250;
-            this.Controls.Add(Trackbar);
-            Trackbar_.Location = new Point(10, 400);
-            Trackbar_.Size = new Size(200, 200);
-            Trackbar_.Minimum = 0;
-            Trackbar_.Maximum = 100;
-            Trackbar_.ValueChanged += (sender, e) =>
-            {
-                label_speed.Text = "Скорость (мс): "+Trackbar_.Value.ToString();
-            };
-            this.Controls.Add(Trackbar_);
-        }
-        private void DrawButton()
-        {
-            Generate.Text = "Генерация чисел";
-            Generate.Size = new Size(100, 50);
-            Generate.Location = new Point(670, 10);
-            this.Controls.Add(Generate);
-            Generate.Click += (sender, e) =>
-            {
-                RCB.Clear();
-                Data.Series[0].Points.Clear();
-                P.Start_work(Trackbar.Value);
-                RCB.Text += P.Return_String();
-                Array.Sort(P.Numbers);
-                for (int i = 0; i < P.Numbers.Length; i++)
-                {
-                    Data.Series[0].Points.AddXY(i, P.Numbers[i]);
-                }
-            };
-        }
-
-        private void DrawLabel()
-        {
-            label_count.Size = new Size(150, 30);
-            label_count.Location = new Point(520, 10);
-            label_count.Text = "Количество: " + Trackbar.Value.ToString();
-            this.Controls.Add(label_count);
-            label_speed.Size = new Size(150, 30);
-            label_speed.Location = new Point(520, 40);
-            label_speed.Text = "Скорость (мс): " + Trackbar_.Value.ToString();
-            this.Controls.Add(label_speed);
-        }
-
-        private void DrawChart()
-        {
-            Data.Location = new Point(10, 10);
-            Data.Size = new Size(500, 300);
-            this.Controls.Add(Data);
-            Data.ChartAreas.Add(new ChartArea("area"));
-
-            // Настройка типа диаграммы
-            Data.Series.Add(new Series("series"));
-            Data.Series["series"].ChartType = SeriesChartType.Column;
-            Data.Titles.Add("Сортировка");
-
-
-            // Настройка меток осей
-            Data.ChartAreas["area"].AxisX.Title = "Номера элементов";
-            Data.ChartAreas["area"].AxisY.Title = "Значения элементов";
-        }
         
-
         private void Form1_Load(object sender, EventArgs e)
         {
             
--- Визуализатор_сортировки/Presenter.cs
+++ Визуализатор_сортировки/Presenter.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Визуализатор_сортировки
 {
@@ -10,6 +15,132 @@ namespace Визуализатор_сортировки
     {
         public ISort Algorithm;
         private Random r = new Random();
+
+        Chart Data = new Chart();
+        System.Windows.Forms.Label label_count = new System.Windows.Forms.Label();
+        System.Windows.Forms.Label label_speed = new System.Windows.Forms.Label();
+        System.Windows.Forms.Button Generate = new System.Windows.Forms.Button();
+        RichTextBox RCB = new RichTextBox();
+        System.Windows.Forms.TrackBar Trackbar = new System.Windows.Forms.TrackBar();
+        System.Windows.Forms.TrackBar Trackbar_ = new System.Windows.Forms.TrackBar();
+        CheckBox AlBubble = new CheckBox();
+        CheckBox AlInsert = new CheckBox();
+
+        public CheckBox DrawCheckBox1()
+        {
+            AlBubble.Enabled = true;
+            AlBubble.Location = new Point(600, 380);
+            AlBubble.Checked= true;
+            AlBubble.CheckedChanged += (sender, e) =>
+            {
+                if (AlBubble.Checked)  AlInsert.Checked = false; 
+            };
+
+            return AlBubble;
+        }
+
+        public CheckBox DrawCheckBox2()
+        {
+            AlInsert.Enabled = true;
+            AlInsert.Location = new Point(600, 410);
+            AlInsert.CheckedChanged += (sender, e) =>
+            {
+                if (AlInsert.Checked) AlBubble.Checked = false;
+            };
+            return AlInsert;
+        }
+
+
+        public RichTextBox DrawRichTextBox()
+        {
+            RCB.Location = new Point(520, 60);
+            RCB.Size = new Size(250, 300);
+            return RCB;
+        }
+
+        public System.Windows.Forms.TrackBar DrawTrackBar_1()
+        {
+            Trackbar.Location = new Point(10, 340);
+            Trackbar.Size = new Size(500, 200);
+            Trackbar.Minimum = 0;
+            Trackbar.Maximum = 10000;
+            Trackbar.ValueChanged += (sender, e) =>
+            {
+                label_count.Text = "Количество: " + Trackbar.Value.ToString();
+            };
+            Trackbar.TickFrequency = 250;
+            return Trackbar;
+        }
+
+        public System.Windows.Forms.TrackBar DrawTrackBar_2()
+        {
+            Trackbar_.Location = new Point(10, 400);
+            Trackbar_.Size = new Size(300, 200);
+            Trackbar_.Minimum = 0;
+            Trackbar_.Maximum = 1000;
+            Trackbar_.ValueChanged += (sender, e) =>
+            {
+                label_speed.Text = "Скорость (мс): " + Trackbar_.Value.ToString();
+            };
+            return Trackbar_;
+        }
+
+        public System.Windows.Forms.Button DrawButton()
+        {
+            Generate.Text = "Генерация чисел";
+            Generate.Size = new Size(100, 50);
+            Generate.Location = new Point(670, 10);
+            //this.Controls.Add(Generate);
+            Generate.Click += (sender, e) =>
+            {
+                RCB.Clear();
+                Data.Series[0].Points.Clear();
+                Start_work(Trackbar.Value);
+                RCB.Text += Return_String();
+                Array.Sort(Numbers);
+                for (int i = 0; i < Numbers.Length; i++)
+                {
+                    Data.Series[0].Points.AddXY(i, Numbers[i]);
+                }
+            };
+            return Generate;
+        }
+
+        public Chart DrawChart()
+        {
+            Data.Location = new Point(10, 10);
+            Data.Size = new Size(500, 300);
+            Data.ChartAreas.Add(new ChartArea("area"));
+
+            // Настройка типа диаграммы
+            Data.Series.Add(new Series("series"));
+            Data.Series["series"].ChartType = SeriesChartType.Column;
+            Data.Titles.Add("Сортировка");
+
+
+            // Настройка меток осей
+            Data.ChartAreas["area"].AxisX.Title = "Номера элементов";
+            Data.ChartAreas["area"].AxisY.Title = "Значения элементов";
+            return Data;
+        }
+
+        public System.Windows.Forms.Label DrawLabel1()
+        {
+            label_count.Size = new Size(150, 30);
+            label_count.Location = new Point(520, 10);
+            label_count.Text = "Количество: " + Trackbar.Value.ToString();
+            return label_count;
+        }
+
+
+        public System.Windows.Forms.Label DrawLabel2()
+        {
+            label_speed.Size = new Size(150, 20);
+            label_speed.Location = new Point(520, 40);
+            label_speed.Text = "Скорость (мс): " + Trackbar_.Value.ToString();
+            return label_speed;
+        }
+
 
         public double[] Numbers;
         public double[] Start_work(int Lenght)
